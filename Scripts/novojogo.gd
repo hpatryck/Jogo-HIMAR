521eba0fe29902ba97bc50a1e09ed6ae4965ef26
hpatryck <patrycklopes762@gmail.com>
2022-08-31 14:02:15 -0300

    atualizei a inserção no db

--- Scripts/novojogo.gd
+++ Scripts/novojogo.gd
@@ -4,39 +4,37 @@ const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
 var db
 var db_name = "res://SQLite/database.db"
 
-
 func _ready():
 	db = SQLite.new()
 	db.path = db_name
 	db.open_db()
 	db.query("update infojogador set status = '0';")
-	#COMMIT AQUI
-	
+	db.close_db()
+	db.open_db()
 	
 func _insertDB():
 	var name = $TextEdit.get_text().strip_edges()
-	print(name)
 	if name == "":
 		return false
 	db.query_with_bindings("select * from infojogador where nome = ?;", [name])
 	var player = db.query_result
+	
 	if len(player) == 0:
-		db.insert_rows("infojogador", {'nome':name})
-		#COMMIT AQUI
+		db.query_with_bindings("insert into infojogador (nome) values (?);", [name])
+		db.close_db()
+		db.open_db()
 		db.query_with_bindings("select * from infojogador where nome = ?;", [name])
 		player = db.query_result
-	print(player)
-	db.query_with_bindings("update infojogador set status = '1' where id = '?';", [player[0]["id"]])
-	#COMMIT AQUI
+	
+	db.query_with_bindings("update infojogador set status = '1' where id = ?;", [player[0]["id"]])
+	
+	db.close_db()
 	return true
-	
-	
 	
 func _on_TextureButton3_pressed():
 	if _insertDB():
 		get_tree().change_scene("res://Cenas/Epoca.tscn"  )
 	pass # Replace with function body.
-
 
 func _on_TextureButton2_pressed():
 	get_tree().change_scene("res://Cenas/comecar.tscn"  )
